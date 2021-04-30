d23544da7f0708968cd183d4a4fc0dbd7ccf178e
Alan Rosas Palacios <43190667+trimaxlan98@users.noreply.github.com>
2021-04-29 21:47:45 -0500

    Avances codigo Alan
    Ya logre coordinar  el que solo haya 5 clientes en las sillas y solo haya 6 empleados trabajando

--- Alan.c
+++ Alan.c
@@ -8,34 +8,38 @@
 #include <time.h>
 
 sem_t sem_silla;
-sem_t sem_silla_E;
+sem_t sem_corte;
+sem_t sem_e_disponible;//saber si hay estilistas disponibles
+sem_t sem_cliente_listo;
 
 void*Cliente(void*argumento);
 void*Empleado(void*argumento);
 
 
 void main(void){
-    int aux;
+    int aux=0;
 pthread_t  cliente,empleado ;
 
-sem_init(&sem_silla,0,5);
+sem_init(&sem_silla, 0, 5);
+sem_init(&sem_corte, 0, 0);
+sem_init(&sem_e_disponible, 0, 6);
+sem_init(&sem_cliente_listo, 0, 0);
 
 while (aux < 20) {
     if (pthread_create(&cliente, NULL, Cliente, NULL)) {
         printf("Problema en la creacion del hilo\n");
         exit(EXIT_FAILURE);
     }
+    aux++;
 }
-
+aux = 0;
 while (aux < 6) {
     if (pthread_create(&empleado, NULL, Empleado, NULL)) {
         printf("Problema en la creacion del hilo\n");
         exit(EXIT_FAILURE);
     }
+    aux++;
 }
-
-
-printf("Proceso papa %li\n",pthread_self());
 
 if(pthread_join(cliente,NULL)){
        printf("Problema al crear enlace con otro hilo\n");
@@ -46,16 +50,33 @@ if(pthread_join(empleado,NULL)){
      exit(EXIT_FAILURE);
 }
 
-  sem_destroy(&sem_cerillo);
+  sem_destroy(&sem_silla);
+  sem_destroy(&sem_corte);
+  sem_destroy(&sem_e_disponible);
+  sem_destroy(&sem_cliente_listo);
 
   exit(EXIT_SUCCESS);
 }
 
 void*Cliente(void*argumento){
-      printf("Soy el cliente %li \n", pthread_self());
+      sem_wait(&sem_silla);//sentarse en la silla
+      printf("Soy el cliente %li y estoy sentado \n", pthread_self());
+      sem_wait(&sem_e_disponible);
+      sem_post(&sem_silla);
+      printf("Soy el cliente %li y estoy cortandome el cabello \n", pthread_self());
+      sem_post(&sem_cliente_listo);
+      sem_wait(&sem_corte);
       pthread_exit(0);
 }
 void* Empleado(void* argumento) {
-    printf("Soy el Empleado %li \n", pthread_self());
+    while (1) {
+        sleep(10);
+        sem_wait(&sem_cliente_listo);
+        printf("Soy el Empleado %li \n", pthread_self());
+        printf("Estoy cortando el cabello...\n");
+        sleep(2);
+        sem_post(&sem_corte);
+        sem_post(&sem_e_disponible);
+    }
     pthread_exit(0);
 }
