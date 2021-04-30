19c18ede8777f6db1fd89845eeebafdd7d1681b3
Alan Rosas Palacios <43190667+trimaxlan98@users.noreply.github.com>
2021-04-30 08:33:20 -0500

    Modificaciones Alan
    EL programa ya cuenta con todos los procesos, hasta el momento ya funciona correctamente sincronizando todos.
    Falta la condición para elegir que servicios pedirá cada cliente

--- Alan.c
+++ Alan.c
@@ -12,25 +12,40 @@ sem_t sem_corte;
 sem_t sem_disp_corte;//saber si hay estilistas disponibles
 sem_t sem_pintura;
 sem_t sem_disp_pintura;//saber si hay estilistas disponibles
+sem_t sem_manicura;
+sem_t sem_disp_manicura;//saber si hay estilistas disponibles
+sem_t sem_pedicura;
+sem_t sem_disp_pedicura;//saber si hay estilistas disponibles
 sem_t sem_cliente_listo_c;
 sem_t sem_cliente_listo_p;
+sem_t sem_cliente_listo_m;
+sem_t sem_cliente_listo_pe;
 
 void*Cliente(void*argumento);
 void* Empleado_Cortar_Cabello(void*argumento);
 void* Empleado_Pintar_Cabello(void* argumento);
+void* Empleado_Manicura(void* argumento);
+void* Empleado_Pedicura(void* argumento);
 
 
 void main(void){
     int aux=0;
-pthread_t  cliente,empleado_c,empleado_p ;
+pthread_t  cliente,empleado_c,empleado_p,empleado_m,empleado_pe;
 
 sem_init(&sem_silla, 0, 5);
 sem_init(&sem_corte, 0, 0);
 sem_init(&sem_disp_corte, 0, 2);
 sem_init(&sem_pintura, 0, 0);
 sem_init(&sem_disp_pintura, 0, 2);
+sem_init(&sem_manicura, 0, 0);
+sem_init(&sem_disp_manicura, 0, 1);
+sem_init(&sem_pedicura, 0, 0);
+sem_init(&sem_disp_pedicura, 0, 1);
 sem_init(&sem_cliente_listo_c, 0, 0);
 sem_init(&sem_cliente_listo_p, 0, 0);
+sem_init(&sem_cliente_listo_m, 0, 0);
+sem_init(&sem_cliente_listo_pe, 0, 0);
+
 
 while (aux < 20) {
     if (pthread_create(&cliente, NULL, Cliente, NULL)) {
@@ -55,6 +70,14 @@ while (aux < 2) {
     }
     aux++;
 }
+if (pthread_create(&empleado_m, NULL, Empleado_Manicura, NULL)) {
+    printf("Problema en la creacion del hilo\n");
+    exit(EXIT_FAILURE);
+}
+if (pthread_create(&empleado_pe, NULL, Empleado_Pedicura, NULL)) {
+    printf("Problema en la creacion del hilo\n");
+    exit(EXIT_FAILURE);
+}
 
 if(pthread_join(cliente,NULL)){
        printf("Problema al crear enlace con otro hilo\n");
@@ -68,14 +91,27 @@ if (pthread_join(empleado_p, NULL)) {
     printf("Problema al crear enlace con otro hilo\n");
     exit(EXIT_FAILURE);
 }
-
+if (pthread_join(empleado_m, NULL)) {
+    printf("Problema al crear enlace con otro hilo\n");
+    exit(EXIT_FAILURE);
+}
+if (pthread_join(empleado_pe, NULL)) {
+    printf("Problema al crear enlace con otro hilo\n");
+    exit(EXIT_FAILURE);
+}
   sem_destroy(&sem_silla);
   sem_destroy(&sem_corte);
   sem_destroy(&sem_disp_corte);
   sem_destroy(&sem_pintura);
   sem_destroy(&sem_disp_pintura);
+  sem_destroy(&sem_manicura);
+  sem_destroy(&sem_disp_manicura);
+  sem_destroy(&sem_pedicura);
+  sem_destroy(&sem_disp_pedicura);
   sem_destroy(&sem_cliente_listo_c);
   sem_destroy(&sem_cliente_listo_p);
+  sem_destroy(&sem_cliente_listo_m);
+  sem_destroy(&sem_cliente_listo_pe);
 
   exit(EXIT_SUCCESS);
 }
@@ -94,12 +130,19 @@ void*Cliente(void*argumento){
       //if(corte==1)
       sem_wait(&sem_disp_corte);
       sem_wait(&sem_disp_pintura);
-      sem_post(&sem_silla);
+      sem_wait(&sem_disp_manicura);
+      sem_wait(&sem_disp_pedicura);
+
+      sem_post(&sem_silla);// se levanta de la silla
       printf("El cliente %li desocupo la silla  \n", pthread_self());
       sem_post(&sem_cliente_listo_c);
       sem_wait(&sem_corte);
       sem_post(&sem_cliente_listo_p);
       sem_wait(&sem_pintura);
+      sem_post(&sem_cliente_listo_m);
+      sem_wait(&sem_manicura);
+      sem_post(&sem_cliente_listo_pe);
+      sem_wait(&sem_pedicura);
       printf("Soy el cliente %li y termine \n", pthread_self());
       pthread_exit(0);
 }
@@ -124,6 +167,30 @@ void* Empleado_Pintar_Cabello(void* argumento) {
         sleep(2);
         sem_post(&sem_pintura);
         sem_post(&sem_disp_pintura);
+    }
+    pthread_exit(0);
+}
+void* Empleado_Manicura(void* argumento) {
+    while (1) {
+        sleep(5);
+        sem_wait(&sem_cliente_listo_m);
+        printf("Soy el Empleado %li \n", pthread_self());
+        printf("Estoy haciendo la manicura...\n");
+        sleep(2);
+        sem_post(&sem_manicura);
+        sem_post(&sem_disp_manicura);
+    }
+    pthread_exit(0);
+}
+void* Empleado_Pedicura(void* argumento) {
+    while (1) {
+        sleep(5);
+        sem_wait(&sem_cliente_listo_pe);
+        printf("Soy el Empleado %li \n", pthread_self());
+        printf("Estoy haciendo la pedicura...\n");
+        sleep(2);
+        sem_post(&sem_pedicura);
+        sem_post(&sem_disp_pedicura);
     }
     pthread_exit(0);
 }
